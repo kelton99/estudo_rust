7aeb4adbce38f731aa7f565824bc725c8113e09e
kelton99 <keltonfreitas53@gmail.com>
2025-05-23 21:43:42 -0300

    start of guessing game

---
+++ ch1.2_hello_cargo/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, cargo!");
+    println!("Create project using cargo new.");
+    println!("Build project using cargo build.");
+    println!("Build and Rus project using cargo run.");
+    println!("Build project and check for errors using cargo check.");
+    println!("Build project optimized for release using cargo build --release.");
+}
